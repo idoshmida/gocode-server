5a6ccc077c7ed4781cb985053fcec5c39a0a311f
IdoShmida <shmidamail@gmail.com>
2021-05-29 09:40:39 +0300

    allmost finished. just the filtered query ternary operator not work

--- server.js
+++ server.js
@@ -1,19 +1,35 @@
 const express = require("express");
 const app = express();
 const fs = require("fs");
+const { toUnicode } = require("punycode");
 
+//midelware: for bodey read
 app.use(express.json())
 
+//home routing:
 app.get("/", (req, res) => {
-    res.send("hello");
+    res.send("hello ido");
 });
 
+//all products routingwith query :
 app.get("/products", (req, res) => {
-    fs.readFile("products.json", "utf8", (err, products) => {
+  const { title } = req.query;
+  fs.readFile("products.json", "utf8", (err, products) => {
+    const productsArr = JSON.parse(products);
+    if (title ) {
+      const productsFiltered = productsArr.filter(
+        (product) => product.title.includes(title));
+      res.send(productsFiltered ? "no data found"  :  productsFiltered)
+
+      // console.log(productsFiltered);
+
+    } else {
       res.send(products);
+    }
     });
   });
 
+  //specific product routing
   app.get("/products/:id", (req, res) => {
     fs.readFile("products.json", "utf8", (err, products) => {
       const productsArr = JSON.parse(products);
@@ -26,14 +42,74 @@ app.get("/products", (req, res) => {
       }
     });
   });
+//post a new product at the end of the products.json file
+app.post("/products", (req, res) => {
+  
+      fs.readFile("products.json", "utf8", (err, products) => { 
+          const productsArr = JSON.parse(products);
+          productsArr.push({
+            "id":productsArr.length +1,
+            "title":req.body.title,
+            "price":req.body.price,
+            "image":req.body.image,
+            "category":req.body.category,
+            "description":req.body.description
+          })
+          fs.writeFile("products.json", JSON.stringify(productsArr), (err)=> {
+            console.log(err); res.send("secsess of post send")
+          } )
+      })
+    })
 
-// app.post("products/", (req, res) => {
-//     fs.readFile("products.json", "utf8", (err, products) => {
-//         const productsArr = JSON.parse(products);
-//         productsArr.push()
-//     })
+// editing a product
+app.put("/products/:id", (req, res) => {
+  fs.readFile("products.json", "utf8", (err, products) => { 
+    const productsArr = JSON.parse(products);
+    let { title  , price, image, description , category} = req.body; 
+    const { id } = req.params;
 
-//     })
+    const updatedProductsArr = productsArr.map((product) => {
+      if (product.id === +id) { 
+
+        //keeping the products properties in case of missing field in the PUT from the user:
+        if (!title) {title = product.title} 
+        if (!price) {price = product.price} 
+        if (!image) {image = product.image} 
+        if (!description) {description = product.description} 
+        if (!category) {category = product.category} 
+
+        return {
+          ...product,
+          title,
+          price,
+          image,
+          description,
+          category
+        };
+      } else {
+        return product;
+      }
+    })
+    fs.writeFile("products.json", JSON.stringify(updatedProductsArr), (err)=> {
+      console.log(err); res.send("secsess of put send")
+    } )
+})
+})
+
+app.delete("/products/:id", (req, res) => {
+  fs.readFile("products.json", "utf8", (err, products) => { 
+    const productsArr = JSON.parse(products);
+
+    const updatedProductsArr = productsArr.filter((product) => 
+      product.id !== +req.params.id
+    )
+    fs.writeFile("products.json", JSON.stringify(updatedProductsArr), (err)=> {
+      console.log(err); res.send("secsess of delete ")
+    } )
+
+  })
+
+})
 
 
 app.listen(8080);
